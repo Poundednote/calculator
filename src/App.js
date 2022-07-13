044c3115daacb6a3cdd0747a1cebd4139a0c7f53
Poundednote <rayyan.naz.02@gmail.com>
2022-07-13 10:31:10 +0100

    added operator functionality and equals

--- src/App.js
+++ src/App.js
@@ -2,24 +2,66 @@ import React, {Component} from 'react'
 import logo from './logo.svg';
 import './App.css';
 
-var result = 0
-
 class App extends Component {
 
   constructor(props) {
     super(props);
 
       this.state = {
+        lastNum: 0,
         num: 0,
         sign: "",
         result: 0
       }
     }
 
-  changeNum(value) {
+  handleNum = (value) => {
     this.setState({
-      num:value
+      num: value
     });
+  }
+
+  handleOperator = (value) => {
+    this.setState({
+      sign: value,
+      lastNum: this.state.num
+    });
+
+  }
+
+  handleResult = (sign, value) => {
+    if (sign === '+') {
+      let newResult = this.state.lastNum + this.state.num
+      this.setState({
+        num: newResult,
+        result: newResult
+      });
+      console.log(sign);
+    }
+    if (sign === '-') {
+      let newResult = this.state.lastNum - this.state.num
+      this.setState({
+        num: newResult,
+        result: newResult 
+      });
+      console.log(sign);
+    }
+    if (sign === 'x') {
+      let newResult = this.state.lastNum * this.state.num
+      this.setState({
+        num: newResult,
+        result: newResult
+      });
+      console.log(sign);
+    }
+    if (sign === '/') {
+      let newResult = this.state.lastNum / this.state.num
+      this.setState({
+        num: newResult,
+        result: newResult
+      });
+      console.log(sign);
+    }
   }
 
   render() {
@@ -27,7 +69,21 @@ class App extends Component {
       <div className='App'>
         <h1>Calculator</h1>
         <Screen value={this.state.num ? this.state.num : this.state.result} />
-        <Button number={1} changeNum={this.changeNum} />
+        <Button number={0} handleNum={this.handleNum} />
+        <Button number={1}  handleNum={this.handleNum} />
+        <Button number={2}  handleNum={this.handleNum} />
+        <Button number={3}  handleNum={this.handleNum} />
+        <Button number={4}  handleNum={this.handleNum} />
+        <Button number={5}  handleNum={this.handleNum} />
+        <Button number={6}  handleNum={this.handleNum} />
+        <Button number={7}  handleNum={this.handleNum} />
+        <Button number={8}  handleNum={this.handleNum} />
+        <Button number={9}  handleNum={this.handleNum} />
+        <Operator sign='+'  handleOperator={this.handleOperator} />
+        <Operator sign='x'  handleOperator={this.handleOperator} />
+        <Operator sign='-'  handleOperator={this.handleOperator} />
+        <Operator sign='/'  handleOperator={this.handleOperator} />
+        <Equals sign={this.state.sign} num={this.state.num} handleResult={this.handleResult} />
       </div>
     );
 
@@ -50,15 +106,24 @@ class Screen extends Component {
 }
 
 class Button extends Component {
-  constructor(props) {
-    super(props);
-
-  }
-
+  
   render() {
-    return <button onClick={this.props.changeNum(this.props.number)}>{this.props.number}</button>
+    return <button onClick={() => this.props.handleNum(this.props.number)}>{this.props.number}</button>;
   }
 
 }
 
+class Operator extends Component {
+
+  render() {
+    return <button onClick={() => this.props.handleOperator(this.props.sign)}>{this.props.sign}</button>;
+  }
+}
+
+class Equals extends Component {
+  render () {
+    return <button onClick={() => this.props.handleResult(this.props.sign, this.props.num)}>{'='}</button>;
+}
+
+}
 export default App;
